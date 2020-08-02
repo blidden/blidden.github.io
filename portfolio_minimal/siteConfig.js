ebe46ce96e8b3fae65de3f7bc37aa945e7455197
Beau Lidden <b.lidden@gmail.com>
2020-08-02 21:17:58 +1000

    Cleanup

--- portfolio_minimal/siteConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'Beau Lidden',
-  shortName: 'Beau',
-  description: 'Hi, I\'m Beau - a product designer living between the escarpment and the Aussie beaches in NSW, Australia.',
-  url: 'https://beaulidden.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Beau Lidden',
-  twitter: 'hyggeligbo'
-}
